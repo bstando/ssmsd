0dfbbb152cab5635da21f8fcd226714003ab7ae9
bstando <stando01@wp.pl>
2017-11-29 23:26:30 +0100

    Fixed build script

--- build_debian.sh
+++ build_debian.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-if [ ! -d "build_debian" ]; then
-	mkdir build_debian
+BASEDIR=$PWD
+BUILDDIR=$BASEDIR/build_debian
+
+if [ ! -d "$BUILDDIR" ]; then
+	mkdir $BUILDDIR
 fi
 
-cd build
+cd $BUILDDIR
+
 apt install libavahi-client-dev libavahi-common-dev cmake libsqlite3-dev libboost-all-dev avahi-daemon libboost-dev libboost-log-dev libboost-program-options-dev
 
-cmake ..
+cmake $BASEDIR
 
 make
